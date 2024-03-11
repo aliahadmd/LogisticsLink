4fd758cb4c77119536796b4501849143ae974fcf
Ali <aliahadmd1@gmail.com>
2024-03-12 02:31:30 +0600

    setup homeserver and docker init

--- config/database.js
+++ config/database.js
@@ -1,8 +1,16 @@
+// Import Sequelize
 import { Sequelize } from "sequelize";
 
-const sequelize= new Sequelize ('database', process.env.DB_USER, process.env.DB_PASSWORD,{
-    dialect:'sqlite',
-    storage:'database.sqlite'
-})
+// Create a Sequelize instance with PostgreSQL
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // Database user
+  process.env.DB_PASSWORD, // Database password
+  {
+    host: process.env.DB_HOST, // Hostname of the PostgreSQL service in Docker Compose
+    dialect: "postgres", // Specify PostgreSQL as the dialect
+    port: 5432, // Default PostgreSQL port
+  }
+);
 
-export default sequelize
+export default sequelize;
